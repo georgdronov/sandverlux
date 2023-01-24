bde91267971e0470294e44852584cc4849525e18
dimosoff <dimmiostr@mail.ru>
2023-01-25 01:49:49 +0300

    extra style rebase, mobile resp., 404, thank-you

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,12 +1,22 @@
 export const server = () => {
-  app.plugins.browsersync.init({
-    open: false,
-    server: {
-      baseDir: `${app.path.build.html}`,
-      serveStaticOptions: {
-        extensions: ["html"],
+  app.plugins.browsersync.init(
+    {
+      open: false,
+      server: {
+        baseDir: `${app.path.build.html}`,
+        serveStaticOptions: {
+          extensions: ["html"],
+        },
       },
+      port: 3000,
     },
-    port: 3000,
-  });
+    function (err, bs) {
+      bs.addMiddleware("*", function (req, res) {
+        res.writeHead(302, {
+          location: "404",
+        });
+        res.end("Redirecting!");
+      });
+    }
+  );
 };
